5b6fe615f8aaf85c7c6d22265ef7235947e428df
felix_lara <larafelix2197@gmail.com>
2025-09-03 22:56:43 -0500

    Avance en problemas lab 1

--- Lab1.1/src/lab1/pkg1/Lab11.java
+++ Lab1.1/src/lab1/pkg1/Lab11.java
@@ -15,26 +15,23 @@ public class Lab11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         float art1,art2,art3,art4,art5,subtotal;
         final float ITBMS,DESC;
         Scanner r=new Scanner (System.in);
         ITBMS = (float) 0.07;
         DESC = 0.3f;
         System.out.println("Introduce el precio de 5 articulos");
-//        r.useLocale(Locale.US);
         art1=r.nextFloat();
         art2=r.nextFloat();
         art3=r.nextFloat();
         art4=r.nextFloat();
         art5=r.nextFloat();
         subtotal=art1+art2+art3+art4+art5;
-        System.out.printf("El subtotal sin descuento es: %.2f\n", subtotal);
+        System.out.printf("El subtotal sin descuento es: $%.2f\n", subtotal);
         subtotal-=subtotal*DESC;
-        System.out.printf("El subtotal con descuento del %.0f%% es: %.2f\n",DESC*100 ,subtotal);
-        System.out.printf("El impuesto de ITBMS es del: %.0f%%\n", ITBMS*100);
+        System.out.printf("El subtotal con descuento del %.0f%% es: $%.2f\n", DESC * 100, subtotal);
         subtotal+=subtotal*ITBMS;
-        System.out.printf("El subtotal con descuento y agregando el impuesto es: %.2f\n",subtotal);
+        System.out.printf("El total con descuento y agregando el impuesto del 7%% es: $%.2f\n", subtotal);
     }
     
 }
--- Lab1.2/src/lab1/pkg2/Lab12.java
+++ Lab1.2/src/lab1/pkg2/Lab12.java
@@ -16,26 +16,26 @@ public class Lab12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        float art1,art2,art3,art4,art5,subtotal;
-        final float ITBMS,DESC;
-        Scanner r=new Scanner (System.in);
-        ITBMS = (float) 0.07;
-        DESC = 0.3f;
-        System.out.println("Introduce el precio de 5 articulos");
-//        r.useLocale(Locale.US);
-        art1=r.nextFloat();
-        art2=r.nextFloat();
-        art3=r.nextFloat();
-        art4=r.nextFloat();
-        art5=r.nextFloat();
-        subtotal=art1+art2+art3+art4+art5;
-        System.out.printf("El subtotal sin descuento es: %.2f\n", subtotal);
-        subtotal-=subtotal*DESC;
-        System.out.printf("El subtotal con descuento del %.0f%% es: %.2f\n",DESC*100 ,subtotal);
-        System.out.printf("El impuesto de ITBMS es del: %.0f%%\n", ITBMS*100);
-        subtotal+=subtotal*ITBMS;
-        System.out.printf("El subtotal con descuento y agregando el impuesto es: %.2f\n",subtotal);
+        int num1, min;
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("Introduce 3 numeros");
+
+        min = read.nextInt();
+        num1 = read.nextInt();
+
+        if (num1 > min) {
+            num1 = read.nextInt();
+        } else {
+            min = num1;
+            num1 = read.nextInt();
+        }
+
+        if (num1 > min) {
+            System.out.printf("El menor es: %d\n", min);
+        } else {
+            min = num1;
+            System.out.printf("El menor es: %d\n", min);
+        }
     }
-    
 }
