39a30ff52e5ea562477eb8aa002f43a292ca2271
felix_lara <larafelix2197@gmail.com>
2025-09-03 23:06:27 -0500

    Mejora de generalizacion y comentarios en Lab1.2

--- Lab1.2/src/lab1/pkg2/Lab12.java
+++ Lab1.2/src/lab1/pkg2/Lab12.java
@@ -16,26 +16,30 @@ public class Lab12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int num1, min;
+// se definen 2 variables que se compararan
+        int next, min;
 
         Scanner read = new Scanner(System.in);
         System.out.println("Introduce 3 numeros");
 
+// se lee la primera variable
         min = read.nextInt();
-        num1 = read.nextInt();
 
-        if (num1 > min) {
-            num1 = read.nextInt();
-        } else {
-            min = num1;
-            num1 = read.nextInt();
-        }
+// se crea un ciclo para poder generalizar la cantidad de numeros a comparar
+        for (int i = 0; i < 2; i++) {
 
-        if (num1 > min) {
-            System.out.printf("El menor es: %d\n", min);
-        } else {
-            min = num1;
-            System.out.printf("El menor es: %d\n", min);
+// se lee el siguiente numero
+            next = read.nextInt();
+
+// se comparan y si resulta que el siguiente es igual o menor se actualiza el nuevo menor
+            if (next < min) {
+
+                min = next;
+
+            }
+
         }
+// se imprime el numero menor de todos los ingresados
+        System.out.printf("El menor es: %d\n", min);
     }
 }
